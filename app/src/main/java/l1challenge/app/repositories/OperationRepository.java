f8b1bdf3ef2c565739d5f5a0f7b2ce6c2b06e26b
GC <guilleczop@gmail.com>
2021-11-11 21:52:12 -0300

    Se filtran las operaciones por usuario

--- app/src/main/java/l1challenge/app/repositories/OperationRepository.java
+++ app/src/main/java/l1challenge/app/repositories/OperationRepository.java
@@ -1,8 +1,14 @@
 package l1challenge.app.repositories;
 
 import l1challenge.app.Operation;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OperationRepository extends CrudRepository<Operation, Integer> {
+
+    //@Query("select * from operation o where o.wallet in (?1,?2,?3)")
+    public Iterable<Operation> findByWalletIn(List<Integer> walletIds);
 
 }
